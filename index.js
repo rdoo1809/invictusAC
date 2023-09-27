ff4b02d5b39c49c85cf19037f221a23fcb0e8bb7
Ryan <r1d8o0o9@gmail.com>
2023-09-27 14:52:17 -0400

    updates

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+// import the dependencies
 const express = require('express');
+const path = require('path');
 var myApp = express();
 
+// set up views and public folders
+myApp.set('views', path.join(__dirname, 'views'));
+myApp.use(express.static(__dirname+'/public'));
+myApp.set('view engine', 'ejs');
+
+
+
 myApp.get('/', function(req, res){
-    res.send("Helllo BIG WORLD");
+res.render('home'); // no need to add .ejs to the file
+name
 });
+
 
 myApp.listen(3000, function(){
     console.log("Application started and listening on port 3000");
